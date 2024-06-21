7cf5a8c3d119ccef5e6781211e7787794ea708c5
Izzyyz <166703521+Izzyyz@users.noreply.github.com>
2024-06-21 17:36:35 -0500

    X.e 1.2

---
+++ Ft/src/app/resources/asc/asc.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-asc',
+  standalone: true,
+  imports: [],
+  templateUrl: './asc.component.html',
+  styleUrl: './asc.component.css'
+})
+export class AscComponent {
+
+}
