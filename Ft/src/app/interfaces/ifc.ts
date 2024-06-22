e77a097e7d301ae81fde676e3e631a9e979fd150
Izzyyz <166703521+Izzyyz@users.noreply.github.com>
2024-06-21 20:19:01 -0500

    X.e 1.8

--- Ft/src/app/interfaces/ifc.ts
+++ Ft/src/app/interfaces/ifc.ts
@@ -1,6 +1,4 @@
 export interface Ifc {
-    name:String;
     email:String;
     password:String;
-    cpassword:String;
 }
--- Ft/src/app/resources/log/log.component.ts
+++ Ft/src/app/resources/log/log.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Ifc } from "../../interfaces/ifc";
-import {ReactiveFormsModule,FormControl,FormGroup,Validators,} from '@angular/forms';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators, } from '@angular/forms';
 
 
 @Component({
@@ -16,7 +16,19 @@ export class LogComponent {
     password: new FormControl('', Validators.required),
   });
 
-  eventSubmit(){
-    console.log(this.IfcForm)
+  eventSubmit() {
+    if (this.IfcForm.valid) {
+      const email = this.IfcForm.value.email;
+      const password = this.IfcForm.value.password;
+
+      if (typeof email === 'string' && typeof password === 'string') {
+        const Ifc: Ifc = {
+          email,
+          password,
+        };
+        console.log("Info", Ifc)
+    } else {
+      console.log("L");
+    }
   }
-}
+}}
